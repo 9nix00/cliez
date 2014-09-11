af4278894ada8af5c234e969350bd72db6ec7a92
Breeze.kay <dev@kbonez.net>
2014-09-11 12:49:26 +0800

    change docs themes

--- docs/conf.py
+++ docs/conf.py
@@ -268,3 +268,11 @@ texinfo_documents = [
 
 # Example configuration for intersphinx: refer to the Python standard library.
 intersphinx_mapping = {'http://docs.python.org/': None}
+
+
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+
+if not on_rtd:
+    import sphinx_rtd_theme
+    html_theme = 'sphinx_rtd_theme'
+    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
