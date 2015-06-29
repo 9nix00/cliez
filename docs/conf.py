adbc9ec23f5e2447dd8768ea2299d395088de885
Breeze.kay <dev@kbonez.net>
2015-06-29 17:54:28 +0800

    update email info

--- docs/conf.py
+++ docs/conf.py
@@ -68,7 +68,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Cliez'
-copyright = u'2014-2015, WANG WENPEI <wangwenpei@kbonez.com>'
+copyright = u'2014-2015, WANG WENPEI <wangwenpei@nextoa.com>'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
