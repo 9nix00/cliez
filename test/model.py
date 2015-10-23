0df48d7159841f66cc4e8dac8e9a52727c69091e
Breeze.kay <wangwenpei@nextoa.com>
2015-10-23 11:12:23 +0800

    add testcase for settings()

---
+++ test/model.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+import unittest
+from cliez.conf import settings, Settings
+
+
+class Model(object):
+    config_none = settings()
+
+    def __init__(self):
+        self.config = settings()
+        pass
+
+    pass
+
+
+class ModelTestCase(unittest.TestCase):
+    def setUp(self):
+        Settings.bind('cliez.conf')
+        pass
+
+    def test_ok(self):
+        a = Model()
+        self.assertEqual(None, a.config_none)
+        self.assertEqual(None, a.config.PACKAGE_ROOT)
+        pass
+
+    pass
