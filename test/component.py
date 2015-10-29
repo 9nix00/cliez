905c57da453673e4c825677706b96513e05b6db2
Breeze.kay <wangwenpei@nextoa.com>
2015-10-29 12:53:46 +0800

    添加component组建内部方法的测试用例

---
+++ test/component.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+import unittest
+from cliez.conf import settings, Settings
+from cliez.base.component import Component
+
+
+class ComponentTestCase(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test_load_resource(self):
+        """
+        资源加载测试,正常情况以下功能不抛出异常
+        :return:
+        """
+        a = Component()
+        a.load_resource('component.py', root='cliez/base')
+        a.load_resource('base/component.py', root='cliez')
+        a.load_resource('/base/component.py', root='cliez')
+        a.load_resource('cliez/base/component.py')
+        a.load_resource(__file__.rsplit('/', 2)[0] + '/cliez/base/component.py')
+        pass
+
+    pass
