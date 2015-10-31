88e82f8a305151983a2274449786cc1a6cbeb850
Breeze.kay <wangwenpei@nextoa.com>
2015-10-31 14:25:39 +0800

    更新默认生成的版本号为0.1

--- cliez/component/init.py
+++ cliez/component/init.py
@@ -23,8 +23,8 @@ class InitComponent(Component):
 
         open(os.path.join(pkg_path, '__init__.py'), 'w').write('''# -*- coding: utf-8 -*-
 
-version = '0.1b1'
-version_info = (0, 1, -1)
+version = '0.1'
+version_info = (0, 1, 0)
 
 ''')
 
