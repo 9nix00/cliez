5b2dac6b49d26dbe60a39f90f87f0640bad04cf7
Breeze.kay <wangwenpei@nextoa.com>
2015-11-01 12:38:09 +0800

    添加flask的底层单元测试模块

---
+++ cliez/flaskez/unittest.py
@@ -0,0 +1,116 @@
+# -*- coding: utf-8 -*-
+
+"""
+基于此例测试用例的使用限制
+
+* 每一个测试用例只能支持一种数据库,也就是说,在我们规划blueprint或者flask app时,只能存在一个数据源.
+    多个数据源的场景,只能要分割为多个bluesprint. 这总体是可接受的,至少对我来说是可接受的
+
+
+* 目前只支持对peewee和mongoengine的数据库自动化测试,加载方法:
+    需要创建自身的database和database_test
+
+
+"""
+
+
+class TestCase(unittest.TestCase):
+    db = None
+    db_type = None
+    db_name = None
+
+    @classmethod
+    def setUpClass(cls):
+        """
+        底层测试用例初始化
+        目前支持对 `mongoengine` 和 `peewee.MySQLDatabase` 的数据库测试
+
+        如果是mongodb,尝试重建数据库
+
+        如果是peewee,尝试执行用户自定义的 `db_patch`
+
+
+        :return:
+        """
+
+        super(TestCase, cls).setUpClass()
+
+        app = settings().app
+        app.config.from_object(settings().DevelopmentConfig)
+        app.config['TESTING'] = True
+
+        db = app.config.get('DATABASE')
+        cls.app = app.test_client()
+
+        if db:
+            if db.__class__.__module__ == 'peewee' and db.__class__.__name__ == 'MySQLDatabase':
+                cls.db_type = 'mysql'
+                cls.db = db
+                cls.db_name = db.database + '_test'
+                db.database = cls.db_name
+
+                app.config['DATABASE'] = cls.db
+
+                try:
+                    import importlib
+                    mod = importlib.import_module(Settings._db_patch_path)
+
+                    for m in mod.__all__:
+                        patch = importlib.import_module(Settings._db_patch_path + '.' + m)
+                        patch.commit()
+                        pass
+
+                except ImportError:
+                    pass
+
+                pass
+            elif cls.db.__module__ == 'pymongo.mongo_client':
+                import mongoengine
+                mongo_settings = mongoengine.connection._connection_settings.get('default')
+                if mongo_settings:
+                    mongo_settings['name'] += '_test'
+                    mongodb_handler = mongoengine.connect(**mongo_settings)
+                    mongodb_use_db = mongo_settings['name']
+                    mongodb_handler.drop_database(mongodb_use_db)
+                    cls.db = mongodb_handler
+                    cls.db_type = 'mongodb'
+                    cls.db_name = mongodb_use_db
+                pass
+            pass
+        pass
+
+    @classmethod
+    def tearDownClass(cls):
+        """
+        底层测试用例初始化还原
+        目前支持对 `mongoengine` 和 `peewee.MySQLDatabase` 的数据库测试
+
+        如果是mongodb,则直接删除数据库
+
+        如果是peewee,倒序执行revert
+
+        :return:
+        """
+
+        if cls.db_type == 'mongodb':
+            cls.db.drop_database(cls.db_name)
+        elif cls.db_type == 'mysql':
+            try:
+                import importlib
+                mod = importlib.import_module(Settings._db_patch_path)
+
+                for m in reversed(mod.__all__):
+                    patch = importlib.import_module(Settings._db_patch_path + '.' + m)
+                    if hasattr(patch, 'revert'):
+                        patch.revert()
+                    pass
+
+            except ImportError:
+                pass
+            pass
+        pass
+
+        super(TestCase, cls).tearDownClass()
+        pass
+
+    pass
