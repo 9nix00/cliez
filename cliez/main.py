ef39239cb72213be3f0c8c483be922e820b38208
WANG WENPEI <wangwenpei@nextoa.com>
2017-12-31 00:49:53 +0800

    pep-8 format

--- cliez/main.py
+++ cliez/main.py
@@ -1,30 +1,35 @@
 # -*- coding: utf-8 -*-
 
-import os
 import argparse
+import os
+
 from cliez import conf, version
 from cliez.parser import parse
 
 conf.COMPONENT_ROOT = os.path.dirname(__file__)
 conf.GENERAL_ARGUMENTS = [
-    (('--dir',), dict(nargs='?', default=os.getcwd(), help='set working directory')),
+    (('--dir',),
+     dict(nargs='?', default=os.getcwd(), help='set working directory')),
     (('--debug',), dict(action='store_true', help='open debug mode')),
-    (('--dry-run',), dict(action='store_true', help='print command instead execute it')),
+    (('--dry-run',),
+     dict(action='store_true', help='print command instead execute it')),
     (('--verbose', '-v'), dict(action='count')),
 ]
-conf.EPILOG = 'You can submit issues at: https://www.github.com/wangwenpei/cliez'
+conf.EPILOG = 'You can submit issues at: ' \
+              'https://www.github.com/wangwenpei/cliez'
 
 
 def main():
     parser = argparse.ArgumentParser(
-            formatter_class=argparse.RawDescriptionHelpFormatter,
-            epilog=conf.EPILOG,
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+        epilog=conf.EPILOG,
     )
 
     for v in conf.GENERAL_ARGUMENTS:
         parser.add_argument(*v[0], **v[1])
 
-    parser.add_argument('--version', '-V', action='version', version='%(prog)s v{}'.format(version))
+    parser.add_argument('--version', '-V', action='version',
+                        version='%(prog)s v{}'.format(version))
     parse(parser)
     pass
 
