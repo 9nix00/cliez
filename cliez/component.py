6154f3b15a73c217b82d3a91efefc9446c4e9fe5
WANG WENPEI <wangwenpei@nextoa.com>
2017-12-31 16:24:51 +0800

    improve document

--- cliez/component.py
+++ cliez/component.py
@@ -1,10 +1,19 @@
-# -*- coding: utf-8 -*-
+"""
+=============
+Component
+=============
 
+A component is used to constuct user command.
+
+"""
+
+import logging
 import os
 import sys
-import logging
-import termcolor
 import time
+
+import termcolor
+
 from cliez import conf
 
 
@@ -13,19 +22,11 @@ class Component(object):
     logger_name = None
     logger = None
 
-    def __init__(self, parser=None, options=None, settings=None, *args, **kwargs):
+    def __init__(self, parser=None, options=None, settings=None,
+                 *args, **kwargs):
         """
-
         Don't overwrite this method.
-
-        in most case,you should custom  `run()` method
-
-        :param parser:
-        :type parser:  `argparse.ArgumentParser`
-        :param settings: a settings created by `cliez.parser.parse`
-        :type settings: module
-        :param args: `tuple`
-        :param kwargs: `dict`
+        In most case,you should custom  `run()` method
         """
 
         self.parser = parser
@@ -33,183 +34,174 @@ class Component(object):
         self.settings = settings
 
         if not self.logger_name:
-            self.logger_name = self.__class__.__name__.lower().replace('component', '')
+            self.logger_name = 'component.{}'.format(
+                self.__class__.__name__.lower().replace('component', ''))
 
         self.logger = logging.getLogger(self.logger_name)
         pass
 
-    def print_message(self, message, file=None):
+    def print_message(self, message, fh=None):
         """
-        print message when user logging level is not INFO
+        print message on screen
 
-        :param message: message to print
-        :type message: `str`
-        :param file: file to write,default is  `sys.stdout`
-        :type file: fd
+        :param str message:
+        :param file fh: file handle,default is None
         :return: None
         """
-        if message.strip():
-            self.logger.info(message)
-
-        if self.logger.level != logging.INFO:
-            return self.parser._print_message(message + "\n", file)
-        pass
+        return self.parser._print_message(message + "\n", fh)
 
     def print_loading(self, wait, message):
         """
         print loading message on screen
 
         .. note::
+
             loading message only write to `sys.stdout`
 
 
-        :param wait: wait seconds
-        :type wait: `int`
-        :param message: message to print
-        :type message: `str`
+        :param int wait: seconds to wait
+        :param str message: message to print
         :return: None
         """
         tags = ['\\', '|', '/', '-']
 
         for i in range(wait):
             time.sleep(0.25)
-            sys.stdout.write("{}... {}\r".format(message, tags[i % 4]))
+            sys.stdout.write("%(message)s... %(tag)s\r" % {
+                'message': message,
+                'tag': tags[i % 4]
+            })
+
             sys.stdout.flush()
             pass
 
-        sys.stdout.write("{}... Done...\n".format(message))
+        sys.stdout.write("%s... Done...\n" % message)
         sys.stdout.flush()
         pass
 
-    def warn_message(self, message, file=None, prefix="[warn]:", suffix="..."):
+    def warn_message(self, message, fh=None, prefix="[warn]:", suffix="..."):
         """
-        print warn type message
+        print warn type message,
         if file handle is `sys.stdout`, print color message
 
 
-        :param message: message to print
-        :type message: `str`
-        :param file: file handle,default is `sys.stdout`
-        :type file: fd
-        :param prefix: message prefix,default is `[warn]`
-        :type prefix: `str`
-        :param suffix: message suffix ,default is `...`
-        :type suffix: `str`
+        :param str message: message to print
+        :param file fh: file handle,default is `sys.stdout`
+        :param str prefix: message prefix,default is `[warn]`
+        :param str suffix: message suffix ,default is `...`
         :return: None
         """
 
         msg = prefix + message + suffix
+        fh = fh or sys.stdout
 
-        file = file or sys.stdout
-
-        self.logger.warn(message)
-
-        if file is sys.stdout:
-            if self.logger.level != logging.WARNING:
-                termcolor.cprint(msg, color="yellow")
+        if fh is sys.stdout:
+            termcolor.cprint(msg, color="yellow")
         else:
-            file.write(msg)
+            fh.write(msg)
 
         pass
 
     def warn(self, *args, **kwargs):
         """
         alias for `warn_message`
-        :param args:
-        :param kwargs:
-        :return:
         """
 
         return self.warn_message(*args, **kwargs)
 
-    def error_message(self, message, file=None, prefix="[error]:", suffix="..."):
+    def error_message(self, message, fh=None, prefix="[error]:",
+                      suffix="..."):
         """
         print error type message
         if file handle is `sys.stderr`, print color message
 
-        :param message: message to print
-        :type message: `str`
-        :param file: file handle, default is  `sys.stdout`
-        :type file: fd
-        :param prefix: message prefix,default is `[error]`
-        :type prefix: `str`
-        :param suffix: message suffix ,default is `...`
-        :type suffix: `str`
+        :param str message: message to print
+        :param file fh: file handle, default is `sys.stdout`
+        :param str prefix: message prefix,default is `[error]`
+        :param str suffix: message suffix ,default is '...'
         :return: None
         """
 
         msg = prefix + message + suffix
+        fh = fh or sys.stderr
 
-        file = file or sys.stderr
-
-        self.logger.critical(message)
-
-        if file is sys.stderr:
+        if fh is sys.stderr:
             if self.logger.level != logging.CRITICAL:
                 termcolor.cprint(msg, color="red")
         else:
-            file.write(msg)
+            fh.write(msg)
 
         pass
 
-    def error(self, message=''):
+    def error(self, message=None, exit_code=2):
         """
         print error message and exit.
+        this method call `argparser.exit` to exit.
 
-        this method call `argparser.error`
+        :param str message: message to print
+        :param int exit_code: exit code,default is 2
 
-        the default exit status is 2
-
-        :param message: message to print
-        :type message: `str`
-
-        :return:None
+        :return: None
         """
 
-        if self.logger.handlers:
-            self.logger.error(message)
+        return self.parser.exit(exit_code, message)
 
-        return self.parser.error(message)
+    def system(self, cmd, fake_code=False):
+        """
+        a built-in wrapper make dry-run easier.
+        you should use this instead use `os.system`
 
-    def system(self, cmd, fake_code=0):
+        .. note::
+
+            to use it,you need add '--dry-run' option in
+            your argparser options
+
+
+        :param str cmd: command to execute
+        :param bool fake_code: only display command when is True,default is False
+        :return:
+        """
         try:
             if self.options.dry_run:
                 def fake_system(cmd):
                     self.print_message(cmd)
                     return fake_code
+
                 return fake_system(cmd)
         except AttributeError:
-            # options dry-run not exists
+            self.logger.warnning("fake mode enabled,"
+                                 "but you don't set '--dry-run' option "
+                                 "in your argparser options")
             pass
 
-        self.logger.debug(cmd)
         return os.system(cmd)
 
     @staticmethod
     def load_resource(path, root=''):
         """
+        .. warning::
 
-        load resource file in package.
+            Experiment feature.
 
+            BE CAREFUL! WE MAY REMOVE THIS FEATURE!
+
+
+        load resource file which in package.
         this method is used to load file easier in different environment.
-
-
-        .. note::
-
-            experiment feature.
-            this feature only work in python3
-
 
         e.g:
 
-        if we need load resource file `resource.io` from package `cliez.conf` .the easiest way may like this:
+        consume we have a file named `resource.io` in package `cliez.conf`,
+        and we want to load it.
+        the easiest way may like this:
 
         .. code-block:: python
 
             open('../conf/resource.io').read()
 
 
-        An obvious question is if we change working directory. `..` is relative path. it will cause error.
+        An obvious problem is `..` is relative path.
+        it will cause an error.
 
         `load_resource` is designed for solve this problem.
 
@@ -228,14 +220,13 @@ class Component(object):
 
         .. note::
 
-            if you use python3, the document charset *must* be utf-8
+            The document charset *MUST BE* utf-8
 
 
-        :param path: file path
-        :type path: `str`
-        :param root: root path
-        :type root: `str`
-        :return: `str`
+        :param str path: file path
+        :param str root: root path
+
+        :return: str
         """
 
         if root:
@@ -257,7 +248,7 @@ class Component(object):
                     buf = buf.decode('utf-8')
                     pass
             except AttributeError:
-                buf = 'document not work in python2'
+                # load resource feature not work in python2
                 pass
 
         return buf
@@ -265,12 +256,18 @@ class Component(object):
     @staticmethod
     def load_description(name, root=''):
         """
-        load resource file as description but ignore `IOError` and `ImportError`
+        .. warning::
 
-        :param path: name resource path
-        :type path: `str`
-        :param root: same as `load_resource()` root
-        :type root: `str`
+            Experiment feature.
+
+            BE CAREFUL! WE MAY REMOVE THIS FEATURE!
+
+
+        Load resource file as description,
+        if resource file not exist,will return empty string.
+
+        :param str path: name resource path
+        :param str root: same as `load_resource` root
         :return: `str`
         """
         desc = ''
@@ -284,6 +281,12 @@ class Component(object):
 
     @staticmethod
     def hump_to_underscore(name):
+        """
+        Convert Hump style to underscore
+
+        :param name: Hump Character
+        :return: str
+        """
         new_name = ''
 
         pos = 0
@@ -301,12 +304,24 @@ class Component(object):
 
     @classmethod
     def append_arguments(cls, sub_parsers):
-        entry_name = cls.hump_to_underscore(cls.__name__).replace('_component', '')
+        """
+        Add class options to argparser options.
+
+        :param sub_parsers:
+        :return: None
+        """
+
+        entry_name = cls.hump_to_underscore(cls.__name__).replace('_component',
+                                                                  '')
+
+        # set sub command document
         epilog = conf.EPILOG if conf.EPILOG else 'This tool generate by `cliez` https://www.github.com/wangwenpei/cliez'
 
-        sub_parser = sub_parsers.add_parser(entry_name, help=cls.__doc__, epilog=epilog)
+        sub_parser = sub_parsers.add_parser(entry_name, help=cls.__doc__,
+                                            epilog=epilog)
         sub_parser.description = cls.add_arguments.__doc__
 
+        # add slot arguments
         if hasattr(cls, 'add_slot_args'):
             slot_args = cls.add_slot_args() or []
             for v in slot_args:
@@ -327,6 +342,9 @@ class Component(object):
 
     @classmethod
     def add_arguments(cls):
+        """
+        User can overwrite this method add custom options
+        """
         pass
 
     pass
