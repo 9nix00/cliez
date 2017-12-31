8baa7b15f244fb47a1f39ed5442c828a8a8c4975
WANG WENPEI <wangwenpei@nextoa.com>
2017-12-31 14:21:00 +0800

    improve parser document

--- cliez/parser.py
+++ cliez/parser.py
@@ -1,16 +1,31 @@
-# -*- coding: utf-8 -*-
+"""
+=============
+Parser
+=============
 
-import sys
-import os
+Parser is used to handle components.
+
+"""
+
 import importlib
 import logging
+import os
+import sys
+from logging import config as logging_config
+
 from cliez.conf import Settings
 
 
 def command_list():
     """
-    find sub-parser full list.
-    depends on `cliez.COMPONENT_ROOT` path
+    Get sub-command list
+
+    .. note::
+
+        Don't use logger handle this function errors.
+
+        Because the error should be a code error,not runtime error.
+
 
     :return: `list` matched sub-parser
     """
@@ -24,13 +39,15 @@ def command_list():
         pass
 
     if not os.path.exists(root):
-        sys.stderr.write("please set a valid path for `cliez.conf.COMPONENT_ROOT`\n")
+        sys.stderr.write(
+            "please set a valid path for `cliez.conf.COMPONENT_ROOT`\n")
         sys.exit(2)
         pass
 
     try:
         path = os.listdir(os.path.join(root, 'components'))
-        return [f[:-3] for f in path if f.endswith('.py') and f != '__init__.py']
+        return [f[:-3] for f in path if
+                f.endswith('.py') and f != '__init__.py']
     except FileNotFoundError:
         return []
 
@@ -39,53 +56,75 @@ def parse(parser, argv=None, settings_module=None, no_args_func=None):
     """
     parser cliez app
 
-    :param parser: an instance of argparse.ArgumentParser
-    :type parser:  `argparse.ArgumentParser`
+    :param argparse.ArgumentParser parser: an instance
+        of argparse.ArgumentParser
     :param argv: argument list,default is `sys.argv`
-    :type argv: `list` or `tuple`
-    :param settings_module: settings class name, default is `cliez.conf.Settings`
-    :type settings_module: `str`
-    :param function no_args_func: execute when no argument apply.
+    :type argv: list or tuple
 
-    :return: `Component`
+    :param str settings_module: settings class name,
+        default is `cliez.conf.Settings`
+    :param object no_args_func: a callable object.if no sub-parser matched,
+        parser will call it.
 
+    :return:  an instance of `cliez.component.Component` or its subclass
     """
 
     argv = argv or sys.argv
     commands = command_list()
     settings = None if not settings_module else Settings.bind(settings_module)
 
-    assert type(argv) in [list, tuple], TypeError("argv only can be list or tuple")
+    if type(argv) not in [list, tuple]:
+        raise TypeError("argv only can be list or tuple")
 
     # match sub-parser
     if len(argv) >= 2 and argv[1] in commands:
         sub_parsers = parser.add_subparsers()
         class_name = argv[1].capitalize() + 'Component'
-        from cliez.conf import COMPONENT_ROOT
+
+        from cliez.conf import COMPONENT_ROOT, LOGGING_CONFIG
+
         sys.path.insert(0, os.path.dirname(COMPONENT_ROOT))
-        mod = importlib.import_module('{}.components.{}'.format(os.path.basename(COMPONENT_ROOT), argv[1]))
+        mod = importlib.import_module(
+            '{}.components.{}'.format(os.path.basename(COMPONENT_ROOT),
+                                      argv[1]))
+
+        # dynamic load component
         klass = getattr(mod, class_name)
         klass.append_arguments(sub_parsers)
         options = parser.parse_args(argv[1:])
 
-        logging.basicConfig(format='[%(levelname)s]:%(message)s', level=logging.ERROR)
+        obj = klass(parser, options=options, settings=settings)
 
-        if hasattr(options, 'debug') and options.debug:
-            logging.basicConfig(format='[%(levelname)s]:%(message)s', level=logging.DEBUG)
-            pass
+        # init logger
+        default_logger = LOGGING_CONFIG['loggers'].get('component')
+        logger_level = logging.CRITICAL
 
         if hasattr(options, 'verbose'):
             if options.verbose == 1:
-                logging.basicConfig(format='[%(levelname)s]:%(message)s', level=logging.CRITICAL)
+                logger_level = logging.ERROR
             elif options.verbose == 2:
-                logging.basicConfig(format='[%(levelname)s]:%(message)s', level=logging.WARNING)
+                logger_level = logging.WARNING
             elif options.verbose == 3:
-                logging.basicConfig(format='[%(levelname)s]:%(message)s', level=logging.INFO)
+                logger_level = logging.INFO
+                obj.logger.setLevel(logging.INFO)
             pass
 
-        obj = klass(parser, options=options, settings=settings)
+        if hasattr(options, 'debug') and options.debug:
+            logger_level = logging.INFO
+            pass
+
+        if default_logger:
+            default_logger['level'] = logger_level
+            if logger_level in [logging.INFO, logging.DEBUG]:
+                default_logger['handlers'] = ['stdout']
+            pass
+
+        logging_config.dictConfig(LOGGING_CONFIG)
+        obj.logger.setLevel(logger_level)
+
         obj.run(options)
-        # easier to create unittest case
+
+        # return object to make unit test easy
         return obj
 
     if not parser.description and len(commands):
@@ -95,7 +134,7 @@ def parse(parser, argv=None, settings_module=None, no_args_func=None):
     pass
 
     options = parser.parse_args(argv[1:])
-    if no_args_func:
+    if no_args_func and callable(no_args_func):
         return no_args_func(options)
     else:
         parser._print_message("nothing to do...\n")
