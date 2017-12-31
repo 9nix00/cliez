387791536172eee7810affdbe504d5a22c6aae99
WANG WENPEI <wangwenpei@nextoa.com>
2017-12-31 16:47:08 +0800

    improve document

--- cliez/__init__.py
+++ cliez/__init__.py
@@ -2,6 +2,12 @@
 =========================
 Cliez Mini Framework
 =========================
+
+Cliez is a mini cli framework help you create CLI-APP simple and elegant.
+
+Most CLI tools in our team is wrote by `Cliez`,
+e.g: `Cabric <https://github.com/nextoa/cabric/>`_
+
 """
 
 version = "2.0.12"
