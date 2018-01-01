53e37d82c571a64fa55bc93f98ceb2871034523d
WANG WENPEI <wangwenpei@nextoa.com>
2018-01-01 14:19:01 +0800

    improve parser testcase

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -1,53 +1,67 @@
-from unittest import TestCase, main
 import argparse
 import os
-import cliez
+from unittest import TestCase, main
+
 from cliez import conf, parser, version
 
 
-class NormalTest(TestCase):
+class ParserTests(TestCase):
     def setUp(self):
         self.parser = argparse.ArgumentParser()
-        self.parser.add_argument('--version', action='version', version='%(prog)s v{}'.format(version))
+        self.parser.add_argument('--version', action='version',
+                                 version='%(prog)s v{}'.format(version))
         conf.COMPONENT_ROOT = os.path.dirname(__file__)
 
     pass
 
     def test_ignore_set_root(self):
-        with self.assertRaises(SystemExit):
+        with self.assertRaises(SystemExit) as cm:
             conf.COMPONENT_ROOT = None
             parser.parse(self.parser, argv=['anything'])
             pass
+
+        self.assertEqual(cm.exception.code, 2)
         pass
 
-    # it cloudn't be happen in real world. skip it.
     # def test_none(self):
+    #     """
+    #     it cloudn't be happen in real world.
+    #     skip it.
+    #
+    #     :return:
+    #     """
     #     with self.assertRaises(Exception):
     #         parser.parse(self.parser, argv=[])
     #         pass
     #     pass
 
     def test_allow_show_version(self):
-        with self.assertRaises(SystemExit):  # "will call parser.exit"
+        with self.assertRaises(SystemExit) as cm:
             parser.parse(self.parser, argv=['command', '--version'])
+
+        self.assertEqual(cm.exception, 0)
         pass
 
     def test_no_value(self):
-        with self.assertRaises(AttributeError):
-            result = parser.parse(self.parser, argv=['command'], no_args_func=lambda option: option.version)
-            pass
+        result = parser.parse(self.parser, argv=['command'],
+                              no_args_func=lambda option: 'ok')
+
+        self.assertEqual('ok', result)
         pass
 
     def test_no_args_func(self):
         self.parser.add_argument('--port', nargs='?', type=int, default=8000)
-        result = parser.parse(self.parser, argv=['command'], no_args_func=lambda option: option.port)
+        result = parser.parse(self.parser, argv=['command'],
+                              no_args_func=lambda option: option.port)
         self.assertEqual(result, 8000)
         pass
 
     def test_active_show_invalid(self):
-        with self.assertRaises(SystemExit):  # "unknow params"
+        with self.assertRaises(SystemExit) as cm:
             result = parser.parse(self.parser, argv=['command', '--port'])
             self.assertEqual(result, version)
+
+        self.assertEqual(cm.exception.code, 2)
         pass
 
     pass
